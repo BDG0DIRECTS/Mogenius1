b9dbb7a62a8ac9e5a9d8219417bb4f7d78bebaa0
BDG0DIRECTS <134042806+BDG0DIRECTS@users.noreply.github.com>
2023-05-19 12:51:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
    return 'GreyMatters_Bot'
 
 #Ex https://Greymattersbot:ghp_147bkkabcdefgh@github.com/Greymattersbot/Mogenius
-os.system("git clone https://GITHUB-USERNAME:token@github.com/GITHUB-USERNAME/YOUR-REPO-NAME okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 main.py &")
+os.system("git clone https://BDG0DIRECTS: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@github.com/BDG0DIRECTS/Filestore okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 main.py &")
